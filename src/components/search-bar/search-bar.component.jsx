a01379b34d562d8d3f5dbc6233f205a1fe8b1c2a
sam2305 <smath045@fiu.edu>
2021-02-28 01:57:58 -0500

    Complete Project Tutorial (#1)

---
+++ src/components/search-bar/search-bar.component.jsx
@@ -0,0 +1,5 @@
+import './search-bar.styles.css';
+
+export const SearchBar = ({placeholder, handleChange}) => (
+    <input className="search" type="search" placeholder={placeholder} onChange= {handleChange}/>
+)
